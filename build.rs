7ca9a052a9fa728b753745c7f6efe82c96d076fc
Nick Hahn <nick.hahn@posteo.de>
2025-05-15 12:10:04 +0200

    Integrate cbindgen

---
+++ build.rs
@@ -0,0 +1,23 @@
+extern crate cbindgen;
+
+use std::env;
+
+use cbindgen::Config;
+
+fn main() {
+    let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+
+    cbindgen::Builder::new()
+        .with_config(Config::from_file("cbindgen.toml").unwrap())
+        .with_crate(crate_dir)
+        .generate()
+        .map_or_else(
+            |error| match error {
+                cbindgen::Error::ParseSyntaxError { .. } => {}
+                e => panic!("{:?}", e),
+            },
+            |bindings| {
+                bindings.write_to_file("target/include/fastosc.h");
+            },
+        );
+}
