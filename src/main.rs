19dd3e586b7bddbbcdaf6c8defa4bb974efa3e9c
Nick Hahn <nick.hahn@dbaudio.com>
2025-05-16 17:12:47 +0200

    Use start_thread() in example code

--- src/main.rs
+++ src/main.rs
@@ -17,26 +17,26 @@ fn main() {
         Err(_) => panic!("{}", usage),
     };
 
-    let server = OscServer::new_from_ip(addr).unwrap();
-    let thread_server = server.clone();
-    let server_handle = std::thread::spawn(move || {
-        loop {
-            thread_server.recv().unwrap()
-        }
-    });
+    let mut server = OscServer::new_from_ip(addr).expect("Error creating OscServer");
+    server.start_thread().expect("Error creating thread");
     println!("Thread spawned");
-    server.register_handler(
-        "/test",
-        Box::new(|msg: &OscMessage| println!("Hi, {0}, {1:#?}", msg.addr, msg.args)),
-    );
-    server.register_handler(
-        "/test2",
-        Box::new(|msg: &OscMessage| println!("Hi, {0}, {1:#?}", msg.addr, msg.args)),
-    );
-    server.register_handler(
-        "/test5",
-        Box::new(|msg: &OscMessage| println!("Hi, {0}, {1:#?}", msg.addr, msg.args)),
-    );
-
-    let _ = server_handle.join();
+    server
+        .register_handler(
+            "/test",
+            Box::new(|msg: &OscMessage| println!("Hi, {0}, {1:#?}", msg.addr, msg.args)),
+        )
+        .unwrap();
+    server
+        .register_handler(
+            "/test2",
+            Box::new(|msg: &OscMessage| println!("Hi, {0}, {1:#?}", msg.addr, msg.args)),
+        )
+        .unwrap();
+    server
+        .register_handler(
+            "/test5",
+            Box::new(|msg: &OscMessage| println!("Hi, {0}, {1:#?}", msg.addr, msg.args)),
+        )
+        .unwrap();
+    loop {}
 }
